c4d6dea1200f451529accd53058b162c1c4afe32
sofiiako <19rubisco93@gmail.com>
2021-08-19 23:19:01 +0300

    Created Broadcasting task

---
+++ Numpy/Array Basics/Broadcasting/task.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+rng = np.random.default_rng()
+
+x = np.arange(10)
+x = x.reshape(10, 1)
+y = np.ones(7)
+
+w = rng.integers(2, size=(4, 3))
+w = w.reshape(3, 4)
+z = np.ones((3, 4))
+
+if __name__ == '__main__':
+    print(x + y)
+    print(z * w)
