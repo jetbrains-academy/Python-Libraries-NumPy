d414018c92620c7b49ac3f6d368f452c9a7244ff
sofiiako <19rubisco93@gmail.com>
2021-08-25 19:55:45 +0300

    Added Reading and Writing Files task to section Array Basics

---
+++ Numpy/Array Basics/Reading and Writing Files/tests/test_task.py
@@ -0,0 +1,10 @@
+import unittest
+import numpy as np
+
+from task import arr
+
+
+# todo: replace this with an actual test
+class TestCase(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(arr.shape, (100, 4), msg="Wrong array shape")
