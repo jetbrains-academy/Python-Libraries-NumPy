5ae6ba7990ee136e2b68462d32d7553f64dfbac4
sofiiako <19rubisco93@gmail.com>
2021-09-21 20:38:50 +0300

    Addressed comments to Create an Array from Range task in Array Basics

--- NumPy/Array Basics/Create an Array from Range/tests/test_task.py
+++ NumPy/Array Basics/Create an Array from Range/tests/test_task.py
@@ -7,6 +7,6 @@ class TestCase(unittest.TestCase):
     def test_sine(self):
         a, b = sine_array(0, 2 * pi, 100)
         self.assertEqual(round(a[-1], 8), 6.28318531, msg="Ooops")
-        self.assertEqual(b[-1], -2.4492935982947064e-16, msg="Ooops")
+        self.assertAlmostEqual(b[-1], 0, msg="Ooops")
         self.assertEqual(a.shape, (100,), msg="Wrong num in array a")
         self.assertEqual(b.shape, (100,), msg="Wrong num in array b")
