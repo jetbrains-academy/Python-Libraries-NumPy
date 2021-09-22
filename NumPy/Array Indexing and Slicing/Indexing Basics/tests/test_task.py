a03acceda224d4e5671cb11bdf096c636ce1edc2
sofiiako <19rubisco93@gmail.com>
2021-09-22 13:45:04 +0300

    Addressed comments to Indexing Basics task in Array Indexing and Slicing

--- NumPy/Array Indexing and Slicing/Indexing Basics/tests/test_task.py
+++ NumPy/Array Indexing and Slicing/Indexing Basics/tests/test_task.py
@@ -7,7 +7,6 @@ from task import a, b, x
 class TestCase(unittest.TestCase):
     def test_arrays_shape(self):
         self.assertEqual(b.shape, (5, 2), msg="Wrong shape of array b")
-        self.assertEqual(type(a), np.int64, msg="a has to be a numpy.int64")
 
     def test_array_content(self):
         self.assertEqual(a, 19, msg="a has to be equal to 19")
