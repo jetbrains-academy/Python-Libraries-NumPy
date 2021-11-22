f72434ff14b1d3ff006b35d579cb518071aa5093
sofiiako <19rubisco93@gmail.com>
2021-11-22 15:05:07 +0300

    Added images and tests to Split Color Channels

--- NumPy/SVD/Split Color Channels/task.py
+++ NumPy/SVD/Split Color Channels/task.py
@@ -9,7 +9,6 @@ green_channel = img_rescaled[:, :, 1]
 blue_channel = img_rescaled[:, :, 2]
 
 if __name__ == '__main__':
-    print(img_rescaled)
     print(red_channel)
     print(green_channel)
     print(blue_channel)
--- NumPy/SVD/Split Color Channels/tests/test_task.py
+++ NumPy/SVD/Split Color Channels/tests/test_task.py
@@ -1,9 +1,18 @@
 import unittest
+import numpy as np
 
-from task import sum
+from task import img_rescaled, red_channel, green_channel, blue_channel
 
 
-# todo: replace this with an actual test
 class TestCase(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(sum(1, 2), 3, msg="adds 1 + 2 to equal 3")
+    def test_red(self):
+        np.testing.assert_array_equal(red_channel, img_rescaled[:, :, 0],
+                                      'Red slice looks incorrect.')
+
+    def test_green(self):
+        np.testing.assert_array_equal(green_channel, img_rescaled[:, :, 1],
+                                      'Green slice looks incorrect.')
+
+    def test_blue(self):
+        np.testing.assert_array_equal(blue_channel, img_rescaled[:, :, 2],
+                                      'Blue channel slice looks incorrect.')
