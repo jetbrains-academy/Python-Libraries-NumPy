9d72314bd4cb158038f6f9be83ec935d33074fa4
sofiiako <19rubisco93@gmail.com>
2021-12-23 22:32:48 +0300

    Addressed comments to Rebuild Diagonal Matrix

--- Projects/SVD/Rebuild Diagonal Matrix/task.py
+++ Projects/SVD/Rebuild Diagonal Matrix/task.py
@@ -14,7 +14,10 @@ Sigma = np.zeros((U.shape[1], Vt.shape[0]))
 np.fill_diagonal(Sigma, s)
 
 if __name__ == '__main__':
+    # Print shape of the diagonal matrix:
     print(Sigma.shape)
+    # Print the norm of the difference between img_gray and the reconstructed SVD product:
     print(linalg.norm(img_gray - U @ Sigma @ Vt))
+    # Check if the reconstructed product is close to the original matrix
     print(np.allclose(img_gray, U @ Sigma @ Vt))
 
