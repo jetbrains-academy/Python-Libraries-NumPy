ccbdb4cdd331538b41664baec1c9d198283d232e
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-09-15 17:41:41 +0200

    fix(arrays of string and unicode values): newline delimiter started causing a typrerror. replaced it with a different one

--- NumPy/Arrays of String and Unicode Values/numpy.char Module/task.py
+++ NumPy/Arrays of String and Unicode Values/numpy.char Module/task.py
@@ -2,11 +2,16 @@ import numpy as np
 
 
 def read_data(file):
-    text = np.loadtxt(file, delimiter='\n', dtype=np.bytes_)
-    return np.char.decode(text)
-
+    # This will result in two elements for each line of text, such as
+    # [b'Some text', b'']. You will need only the first of them,
+    # so use slicing in the TODO below.
+    text = np.loadtxt(file, delimiter='.', dtype=np.bytes_)
+    return np.char.decode(text[:, 0])
 
 if __name__ == '__main__':
     X = read_data('text.txt')
     print(X)
+    # This should NOT be a list:
+    print(X[0])
+    # This should NOT be a numpy.ndarray:
     print(type(X[0]))
